debaf3891726524e9439493ac78a7ede1ad510c3
fayekelmith <faye.12222883@lpu.in>
2024-01-03 12:11:48 +0530

    project setup

---
+++ public/utils/fonts.ts
@@ -0,0 +1,3 @@
+import { Monofett, Poppins } from "next/font/google";
+export const monofett = Monofett({ weight: "400", subsets: ["latin"] });
+export const poppins = Poppins({ weight: "400", subsets: ["latin"] });
